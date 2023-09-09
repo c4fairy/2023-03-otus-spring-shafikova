167058195374fa8a5c30e8baf4d4239926e948b0
Shafikova Dinara <dinashaf93@gmail.com>
2023-09-09 15:01:23 +0300

    hw-08-spring-data-mongo add mongodb to library application

--- hw-08-spring-data-mongo/src/main/java/ru/otus/service/DBService.java
+++
@@ -1,5 +0,0 @@
-package ru.otus.service;
-
-public interface DBService extends BookDBService, CommentDBService {
-
-}
--- hw-08-spring-data-mongo/src/main/java/ru/otus/service/impl/BookServiceImpl.java
+++ hw-08-spring-data-mongo/src/main/java/ru/otus/service/impl/BookServiceImpl.java
@@ -6,10 +6,12 @@ import ru.otus.domain.Author;
 import ru.otus.domain.Book;
 import ru.otus.domain.Comment;
 import ru.otus.domain.Genre;
+import ru.otus.repository.BookRepository;
+import ru.otus.repository.CommentRepository;
 import ru.otus.service.AuthorService;
 import ru.otus.service.BookService;
-import ru.otus.service.DBService;
 import ru.otus.service.GenreService;
+import ru.otus.service.SequenceGeneratorService;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -19,69 +21,70 @@ import java.util.List;
 public class BookServiceImpl implements BookService {
     private final AuthorService authorService;
     private final GenreService genreService;
-    private final DBService dbService;
+    private final SequenceGeneratorService sequenceGeneratorService;
+    private final BookRepository bookRepository;
+    private final CommentRepository commentRepository;
 
     @Override
     public void addBook(String title, String authorName, String genreName) {
         Author author = authorService.getAuthor(authorName);
         Genre genre = genreService.getGenre(genreName);
         Book book = new Book(title, author, genre);
-        dbService.save(book);
+        bookRepository.save(book);
     }
 
     @Override
     public List<Book> findAll() {
-        return dbService.findAll();
+        return bookRepository.findAll();
     }
 
     @Override
     public Book findById(long id) {
-        return dbService.findById(id);
+        return bookRepository.findById(id);
     }
 
     @Override
     public void deleteById(long id) {
-        dbService.findById(id).getComments().forEach(dbService::delete);
-        dbService.deleteById(id);
+        bookRepository.deleteById(id);
     }
 
     @Override
     public long getCount() {
-        return dbService.count();
+        return bookRepository.count();
     }
 
     @Override
     public void updateNameById(long id, String name) {
-        Book book = dbService.findById(id);
+        Book book = bookRepository.findById(id);
         book.setTitle(name);
-        dbService.save(book);
+        bookRepository.save(book);
     }
 
     @Override
     public List<Book> findByName(String name) {
-        return dbService.findAllByTitle(name);
+        return bookRepository.findAllByTitle(name);
     }
 
     @Override
     public void addComment(long bookId, String commentText) {
-        Book book = dbService.findById(bookId);
+        Book book = bookRepository.findById(bookId);
         if (book != null) {
             Comment comment = new Comment(commentText);
-            dbService.save(comment);
+            commentRepository.save(comment);
             book.setComments(addCommentToBookCommentList(book, comment));
-            dbService.save(book);
+            bookRepository.save(book);
         }
     }
 
     @Override
     public List<Comment> findCommentsByBookId(long id) {
-        Book book = dbService.findById(id);
+        Book book = bookRepository.findById(id);
         return book.getComments();
     }
 
     @Override
     public List<Book> findAllBooksByAuthorId(long id) {
-        return dbService.findAllByAuthorId(id);
+        return bookRepository.findAllByAuthorId(id);
     }
 
 
